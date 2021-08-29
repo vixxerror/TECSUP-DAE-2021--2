ce2cce0bcc4527a2dfa52699abf4df9e990a238c
Klebert Manuel Layme Arapa <klebert.layme@tecsup.edu.pe>
2021-08-29 09:32:16 -0500

    lab02

--- lab02/ejercicios/urls.py
+++ lab02/ejercicios/urls.py
@@ -5,5 +5,7 @@ app_name = 'ejercicios'
 urlpatterns = [
   
     path('operaciones',views.operaciones,name='operaciones'),
-    path('respuesta',views.respuesta,name='respuesta')
+    path('respuesta',views.respuesta,name='respuesta'),
+    path('volumencilindro',views.volumencilindro,name='volumencilindro'),
+    path('result',views.result,name='result')
 ]
--- lab02/ejercicios/views.py
+++ lab02/ejercicios/views.py
@@ -9,13 +9,45 @@ def operaciones(request):
     return render(request, 'operaciones.html')
 
 def respuesta(request):
-    a = request.POST['n1']
-    b = request.POST['n2']
-    suma = int(a) + int(b)
+   
+    a = request.POST['Numero1']
+    b = request.POST['Numero2']
+    ope = request.POST['ope']
+
+    if ope == "Sumar":
+        respuesta = int(a) + int(b)
+    elif ope == "Restar":
+        respuesta = int(a) - int(b)
+    else:
+        respuesta = int(a) * int(b)
+  
+    context = {
+        'ope': ope,
+        'a' : a,
+        'b' : b,
+        'resultado' : respuesta
+    }
+  
+    return render(request,'respuesta.html',context)
+   
+def volumencilindro(request):
+    context = {
+        'titulo' : 'Formulario',
+    }
+    return render(request, 'volumencilindro.html')
+
+
+def result(request):
+    from math import pi
+    a = request.POST['diametro']
+    b = request.POST['altura']
+    result = pi * ((float(a)/2)*(float(a)/2) )* float(b)
     context = {
         'a' : a,
         'b' : b,
-        'resultado' : suma
-
+        'result' : result
     }
-    return render(request,'respuesta.html',context)
+    
+    return render(request,'result.html',context)
+
+
