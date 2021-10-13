62501990804e2125d5e5badd88bcc339ae25927d
Klebert Manuel Layme Arapa <64551136+vixxerror@users.noreply.github.com>
2021-10-13 11:04:32 -0500

    Delete lab05 directory

--- lab05/tienda/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from.models import Categoria
-from.models import Producto
-from.models import Cliente
-admin.site.register(Categoria)
-admin.site.register(Producto)
-admin.site.register(Cliente)
